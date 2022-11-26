03a661a0f6959621ab936a6f4ebda75f0d8d3f0a
irenecervillavalle <irenecervillavalle@gmail.com>
2022-11-26 06:55:46 +0000

    Excuse Generator added

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatorio(datos) {
+    return datos[Math.floor(Math.random() * datos.length)];
+  }
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuse = [
+    aleatorio(who) +
+      " " +
+      aleatorio(action) +
+      " " +
+      aleatorio(what) +
+      " " +
+      aleatorio(when)
+  ];
+
+  let pExcuse = document.querySelector(".js-excuse");
+  pExcuse.innerHTML = excuse;
+  console.log(pExcuse);
 };
